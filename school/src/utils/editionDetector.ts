989f37a7cebebcce2c27748e5881143a246c67e4
Elyrene <ryuraevomail@qq.com>
2026-03-01 14:54:23 +0800

    ！

--- school/src/utils/editionDetector.ts
+++ school/src/utils/editionDetector.ts
@@ -6,7 +6,7 @@ import { getCurrentBookName } from './worldbookHelper'
 export type CardEdition = 'elyrene' | 'original' | 'unknown'
 
 /** 唯一版本号来源，更新只改这里 */
-export const GAME_VERSION = 'V2.5EXfix'
+export const GAME_VERSION = 'V2.6'
 
 /**
  * 通过世界书名称关键词检测当前绑定的卡版本
